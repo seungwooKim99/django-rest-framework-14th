4f253b22ed019f3ef8e2ca85aacf4497e77560b4
Seungwoo Kim <sonicdx886@gmail.com>
2021-11-13 23:09:59 +0900

    [refactor]: 코드 리뷰내용 반영

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import user_list, PostList, PostDetail
+from .views import UserList, PostList, PostDetail
 
 urlpatterns = [
-    path('users',user_list),
+    path('users',UserList.as_view()),
     path('posts', PostList.as_view()),
     path('posts/<int:id>', PostDetail.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -4,18 +4,18 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
 from django.contrib.auth.models import User
 from .models import Profile, Post, Follow, File, Comment, Like
 from .serializers import UserSerializer, ProfileSerializer, PostSerializer, FollowSerializer, FileSerializer, CommentSerializer, LikeSerializer
 
-@csrf_exempt
-def user_list(request):
-    if request.method == 'GET':
+class UserList(APIView):
+    def get(self, request, format=None):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         data = JSONParser().parse(request)
         serializer = UserSerializer(data=data)
         if serializer.is_valid():
@@ -33,21 +33,21 @@ class PostList(APIView):
         data = JSONParser().parse(request)
         serializer = PostSerializer(data=data)
         if serializer.is_valid():
-            #serializer.save()
-            print(serializer.data)
+            temp_user = User.objects.get(id=data["user"])
+            serializer.save(user=temp_user)
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
 
 class PostDetail(APIView):
     def get(self, request, id, format=None):
-        post = Post.objects.filter(id=id)
-        serializer = PostSerializer(post, many=True)
+        post = get_object_or_404(Post,pk=id)
+        serializer = PostSerializer(post, many=False)
         return JsonResponse(serializer.data, safe=False)
 
     def put(self, request, id, format=None):
         data = JSONParser().parse(request)
-        post = Post.objects.get(id=id)
+        post = get_object_or_404(Post, pk=id)
         serializer = PostSerializer(post, data=data)
         if serializer.is_valid():
             serializer.save()
@@ -55,24 +55,6 @@ class PostDetail(APIView):
         return JsonResponse(serializer.errors, status=400)
 
     def delete(self, request, id, format=None):
-        post = Post.objects.filter(id=id)
+        post = get_object_or_404(Post, pk=id)
         post.delete()
         return Response({"message: delete success"},)
-
-
-
-"""
-def post_list(request):
-    if request.method == 'GET':
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-"""
